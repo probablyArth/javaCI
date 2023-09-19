760ce58c5369d67ed50fa72743fa80213d0db860
Arth <probablyarth@gmail.com>
2023-09-19 10:55:28 +0530

    update test

--- problems/problem1/Test.java
+++ problems/problem1/Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
     public static void main(String[] args) throws Exception {
-
+        System.out.println("***Running Tests for problem1..***");
         Solution solveSolution = new Solution();
         int[] ans = solveSolution.solve(new int[] { 2, 3, 4, 1, 1, 7, 8 });
         int min, max;
@@ -9,9 +9,13 @@ public class Test {
             min = ans[0];
             max = ans[1];
         } catch (Exception e) {
+            System.err.println("Runtime error");
+            System.exit(1);
             throw new Error("Runtime Error");
         }
         if (min != 1 || max != 8) {
+            System.err.println("Wrong solution!");
+            System.exit(1);
             throw new Exception("Wrong solution!");
         }
         System.out.print("Testcase passed!");
