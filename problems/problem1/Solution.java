448b795d66656bddae20580bc815694c7ca7cfed
Arth <probablyarth@gmail.com>
2023-09-19 11:43:13 +0530

    hapi?

--- problems/problem1/Solution.java
+++ problems/problem1/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int[] solve(int[] arr) {
-        // System.out.println("hi from");
+        System.out.println("hi from");
         return new int[] { 1, 2 };
     }
 }
