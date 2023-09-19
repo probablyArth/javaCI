b576e73284864b7cd2840bcaeb820eb9d479b4d1
Arth <probablyarth@gmail.com>
2023-09-19 10:20:16 +0530

    init

---
+++ problem1/Solution.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public int[] solve(int[] arr) {
+        return new int[] { 1, 2 };
+    }
+}
---
+++ problem1/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+
+        Solution solveSolution = new Solution();
+        int[] ans = solveSolution.solve(new int[] { 2, 3, 4, 1, 1, 7, 8 });
+        int min, max;
+        try {
+            min = ans[0];
+            max = ans[1];
+        } catch (Exception e) {
+            throw new Exception("Runtime error");
+        }
+        if (min != 1 || max != 8) {
+            throw new Exception("Wrong solution!");
+        }
+        System.out.print("Testcase passed!");
+    }
+}
