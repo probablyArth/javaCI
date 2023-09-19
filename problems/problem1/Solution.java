481fd8a1f40c411b4d4ee82e4115a468f5cc48af
Arth <probablyarth@gmail.com>
2023-09-19 10:42:40 +0530

    update sol

--- problems/problem1/Solution.java
+++ problems/problem1/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int[] solve(int[] arr) {
-        // System.out.println("hi from solution");
+        System.out.println("hi from solution");
         return new int[] { 1, 2 };
     }
 }
